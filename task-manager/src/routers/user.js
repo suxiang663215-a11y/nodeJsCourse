60123e8172f3f01f190c54ae911e3e8e99e04c58
frank.su <frank.su@au.harveynorman.com>
2025-10-30 14:51:47 +1100

    Add bcryptjs for password hashing and update user model and routes

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -40,7 +40,7 @@ router.get("/users/:id", (req, res) => {
 
 router.patch("/users/:id", async (req, res) => {
   const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "age"];
+  const allowedUpdates = ["name", "email", "age", "password"];
   const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update)
   );
@@ -49,10 +49,10 @@ router.patch("/users/:id", async (req, res) => {
     return res.status(400).send({ error: "Invalid updates!" });
   }
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
     if (!user) {
       return res.status(404).send();
     }
